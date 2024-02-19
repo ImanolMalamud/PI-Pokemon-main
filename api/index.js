a09bc16ddc7b0c5b2f5182f0edc4e6d9bbbdc3fb
ImanolMalamud <imanolm93@gmail.com>
2024-02-18 21:44:56 -0300

    fixed bugs that came with merging main and master

--- api/index.js
+++ api/index.js
@@ -1,40 +1,3 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-<<<<<<< HEAD
-require('dotenv').config()
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const { typesInDb } = require('./src/controllers/Controllers');
-const port = process.env.PORT || 3001
-
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-	// llamamos los types de la api y los guardamos en la base de datos
-	typesInDb(),
-
-	// Creamos una variable de entorno llamada PORT (le damos valor 3001 localmente).
-	// Heroku despues va a usar un valor propio internamente (distinto) para esa variable PORT.
-	server.listen(port, () => {
-		console.log(`%s listening at ${port}`); // eslint-disable-line no-console
-	});
-=======
 require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
@@ -48,7 +11,6 @@ conn.sync({ force: true }).then(() => {
     // Creamos una variable de entorno llamada PORT (le damos valor 3001 localmente).
     // Heroku despues va a usar un valor propio internamente (distinto) para esa variable PORT.
     server.listen(port, () => {
-      console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+      console.log(`Server listening at ${port}`); // eslint-disable-line no-console
     });
->>>>>>> master
 });
--- api/src/routes/pokemon.js
+++ api/src/routes/pokemon.js
@@ -1,48 +1,45 @@
-const { Router } = require('express');
-const { Pokemon } = require('../db');
-const controlers = require('../controllers/Controllers');
+const { Router } = require("express");
+const { Pokemon } = require("../db");
+const controlers = require("../controllers/Controllers");
 const router = Router();
 
-router.get('/', async (req, res) => {
-	const { name } = req.query;
-<<<<<<< HEAD
-=======
-	
->>>>>>> master
-	try {
-		if (name) {
-			const findPokemon = await controlers.getPokemonsName(name);
-			return res.json(findPokemon);
-		}
-		const allPokemons = await controlers.getAllPokemons()
-		res.json(allPokemons);
-	} catch (error) {
-		res.status(400).json(error.message);
-	}
+router.get("/", async (req, res) => {
+  const { name } = req.query;
+
+  try {
+    if (name) {
+      const findPokemon = await controlers.getPokemonsName(name);
+      return res.json(findPokemon);
+    }
+    const allPokemons = await controlers.getAllPokemons();
+    res.json(allPokemons);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 });
 
-router.get('/:id', async (req, res) => {
-	// Observar que el id del params viene como string.
-	const { id } = req.params;
-	console.log(id)
-	try {
-		const pokeInfo = await controlers.getPokemonById(id);
-		res.json(pokeInfo);
-	} catch (error) {
-		console.log(error);
-		res.status(400).send(error.message);
-	}
+router.get("/:id", async (req, res) => {
+  // Observar que el id del params viene como string.
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const pokeInfo = await controlers.getPokemonById(id);
+    res.json(pokeInfo);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error.message);
+  }
 });
 
-router.post('/', async (req, res) => {
-	const body = req.body;
-	
-	try {
-		const pokeInfo = await controlers.pokeCreate(body);
-		res.status(201).send(pokeInfo);
-	} catch (error) {
-		res.status(400).send(error.message);
-	}
+router.post("/", async (req, res) => {
+  const body = req.body;
+
+  try {
+    const pokeInfo = await controlers.pokeCreate(body);
+    res.status(201).send(pokeInfo);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 module.exports = router;
